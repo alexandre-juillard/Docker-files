323ad909f260a33a028c29598de0b95215424ded
Xelaj <ajuillard69@gmail.com>
2024-01-23 14:54:41 +0100

    feat(login) add authentification page

---
+++ app/contact.php
@@ -0,0 +1,54 @@
+<?php
+
+require_once '/app/env/variables.php';
+//verif qu'on a uploadé une image et pas d'erreur
+if (!empty($_FILES['image']) && $_FILES['image']['error'] === 0){
+    //on verifie la taille du fichier
+    if($_FILES['image']['size'] < 16000000){
+
+         //verif extension du fichier
+        $fileInfo = pathinfo($_FILES['image']['name']);
+
+       //on récupère extension du fichier
+        $extension = $fileInfo['extension'];
+
+        //on définit les extensions autorisées
+        $extensionAllowed = ['jpg','png', 'jpeg', 'webp', 'svg', 'gif'];
+
+        //on vérifie si extension du fichier est autorisé
+        if(in_array($extension, $extensionAllowed)){
+            $fileName = $fileInfo['filename'] . '_' . (new DateTime())->format('Y-m-d_H:i:s') .'.'. $extension;
+            move_uploaded_file($_FILES['image']['tmp_name'], '/app/upload/$fileName');
+        }
+    }
+}
+
+?>
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact | My first app PHP</title>
+    <link rel="stylesheet" href="<?= $cssPath; ?>structure.css">
+</head>
+
+<body>
+    <?php require_once '/app/layout/header.php'; ?>
+    <main>
+        <h1>Votre demande de contact</h1>
+        <?php var_dump($_FILES); ?>
+        <?php if (!empty($_POST['prenom']) && !empty($_POST['nom']) && !empty($_POST['message'])) : ?>
+            <div class="card">
+                <p>Prenom: <?= strip_tags($_POST['prenom']); ?></p>
+                <p>Nom: <?= htmlspecialchars($_POST['nom']); ?></p>
+                <p>Message: <?= $_POST['message']; ?></p>
+            </div>
+        <?php else : ?>
+            <div class="alert alert-danger">Veuillez soumettre le formulaire</div>
+        <?php endif; ?>
+    </main>
+</body>
+
+</html>
---
+++ app/index.php
@@ -0,0 +1,33 @@
+<?php
+require_once '/app/env/variables.php';
+?>
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My first app PHP</title>
+    <link rel="stylesheet" href="<?= $cssPath; ?>structure.css">
+    <link rel="stylesheet" href="<?= $cssPath; ?>index.css">
+</head>
+
+<body>
+    <?php require_once './layout/header.php'; ?>
+    <main>
+        <h1>Hello world</h1>
+        <form action="/contact.php" method="POST" enctype="multipart/form-data">
+            <label for="prenom">Prénom:</label>
+            <input type="text" name="prenom" id="prenom" placeholder="John" required>
+            <label for="nom">Nom:</label>
+            <input type="text" name="nom" id="nom" placeholder="Doe">
+            <label for="message">Votre message:</label>
+            <textarea name="message" id="message" cols="20" rows="5"></textarea>
+            <label for="image">Image:</label>
+            <input type="file" name="image" id="image">
+            <button type="submit" class="btn btn-primary">Envoyer</button>
+        </form>
+    </main>
+</body>
+
+</html>
